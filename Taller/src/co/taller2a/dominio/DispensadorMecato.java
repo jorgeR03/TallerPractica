dee515705217429677631535d2d16e51726f6b4b
Jorge Andres Carmona Ramirez <jorgeandresR03@gmail.com>
2021-09-11 21:36:50 -0500

    TallerPOO21

--- Taller/src/co/taller2a/dominio/DispensadorMecato.java
+++ Taller/src/co/taller2a/dominio/DispensadorMecato.java
@@ -1,4 +1,60 @@
 package co.taller2a.dominio;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DispensadorMecato {
+    public static final int CANTIDADMAXPRODUCTOS =10;
+    private String marca;
+    private List<Mecato> productos;
+
+    public DispensadorMecato(String marca) {
+        this.marca = marca;
+        this.productos = new ArrayList<>();
+    }
+    public Mecato buscar(int codigo){
+        Mecato mecatoBuscado = null;
+        for (Mecato mecato:this.productos) {
+            if(mecato.getCodigo() == codigo){
+                mecatoBuscado = mecato;
+            }break;
+            
+        }
+        return mecatoBuscado;
+    }
+    public boolean sacarUnidad(int codigo){
+
+    }
+    public boolean sacarUnidad(String nombre){
+
+    }
+    public List<String> consultarAgotados(){
+
+    }
+    public int consultarUnidades(int codigo){
+
+
+    }
+    public int consultarTotalUnidades(){
+
+    }
+    public void aumentarStock(int codigo, int carntidad){
+
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public List<Mecato> getProductos() {
+        return productos;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public void setProductos(List<Mecato> productos) {
+        this.productos = productos;
+    }
 }
--- Taller/src/co/taller2a/dominio/Mecato.java
+++ Taller/src/co/taller2a/dominio/Mecato.java
@@ -1,4 +1,45 @@
 package co.taller2a.dominio;
 
 public class Mecato {
+    private int codigo;
+    private String nombre;
+    private int precio;
+    private int cantidadActual;
+
+    public Mecato(int codigo, String nombre, int precio) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    public Mecato(int codigo, String nombre, int precio, int cantidadActual) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.cantidadActual = cantidadActual;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public int getCantidadActual() {
+        return cantidadActual;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    public void setCantidadActual(int cantidadActual) {
+        this.cantidadActual = cantidadActual;
+    }
 }
